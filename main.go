af993f4f738dba04f5644e1555da8e094437f195
Jose6348 <jfalasco88@gmail.com>
2025-02-13 23:33:05 -0300

    Ponteiros

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	var x *int
+	x = nil
+	take(x)
+	fmt.Println(x)
+
+	// x := create()
+	// fmt.Println(*x)
 }
+
+func take(x *int) {
+	*x = 100
+}
+
+// func create() *int {
+// 	x := 10
+// 	return &x
+// }
+
+// func foo(x *int) {
+// 	*x = 100
+// }
+
+// func take(x *int) {
+// 	*x = 100
+// }
+
+// p = 0x123
+// p --> 0x123 = ?
